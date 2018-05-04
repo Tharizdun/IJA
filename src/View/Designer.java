6d38099e200d5c969df1017678c045a0ed340665
Tharizdun <martin.zednicek@gmail.com>
2018-05-04 20:28:01 +0200

    edit

--- src/View/Designer.java
+++ src/View/Designer.java
@@ -1,8 +1,5 @@
 package View;
 
-import Classes.AddBlock;
-import Classes.Block;
-import Classes.Scheme;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
